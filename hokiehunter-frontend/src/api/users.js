a173b19aad49fffcbbcbb4e457660444859ca86b
harshapaladugu1 <109922324+harshapaladugu1@users.noreply.github.com>
2025-04-15 12:20:43 -0400

    Update users.js

--- hokiehunter-frontend/src/api/users.js
+++ hokiehunter-frontend/src/api/users.js
@@ -1,9 +1,23 @@
 import axios from 'axios';
-
 const BASE_URL = 'http://localhost:5000';
 
 export async function getUsers() {
   const res = await axios.get(`${BASE_URL}/users`);
+  return res.data;
+}
+
+export async function getUser(id) {
+  const res = await axios.get(`${BASE_URL}/users/${id}`);
+  return res.data;
+}
+
+export async function createUser(data) {
+  const res = await axios.post(`${BASE_URL}/users`, data);
+  return res.data;
+}
+
+export async function updateUser(id, data) {
+  const res = await axios.put(`${BASE_URL}/users/${id}`, data);
   return res.data;
 }
 
